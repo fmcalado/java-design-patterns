5974f95d7e7eec9cf352d8ed8d5c63ef08af8113
Calado <wishsolidor@gmail.com>
2022-04-10 18:20:23 -0300

    finish singleton example

--- src/main/java/com/fmcalado/singleton/Demo.java
+++ src/main/java/com/fmcalado/singleton/Demo.java
@@ -3,12 +3,23 @@ package com.fmcalado.singleton;
 import com.fmcalado.singleton.database.DataSource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         DataSource dataSource = DataSource.getInstance();
         Connection connection = dataSource.conn;
+        PreparedStatement pst;
 
+        String sqlQuery = "insert into users(name) values(?)";
+        pst = connection.prepareStatement(sqlQuery);
+        pst.setString(1, "Calado");
+
+        int created = pst.executeUpdate();
+        if(created > 0){
+            System.out.println("User was successfully created");
+        }
 
     }
 }
